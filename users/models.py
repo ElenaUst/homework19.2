a49fc46475cfb5a8bf06eec04f2213481b61e431
ElenaUst <elka_us@mail.ru>
2024-01-11 22:34:43 +0300

    change model User

--- users/models.py
+++ users/models.py
@@ -9,7 +9,8 @@ class User(AbstractUser):
     phone = models.CharField(max_length=35, verbose_name='телефон', blank=True, null=True)
     avatar = models.ImageField(upload_to='users/', verbose_name='аватар', blank=True, null=True)
     country = models.CharField(max_length=100, verbose_name='страна', blank=True, null=True)
-
+    email_verified = models.BooleanField(default=False)
+    email_verification_token = models.CharField(max_length=250, verbose_name='токен пользователя', blank=True, null=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
